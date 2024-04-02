911ffa452dd19a5f10413581c2d3a53f733310b1
Fabrice <fabrice.ndagijimana@gmail.com>
2024-04-03 00:33:29 +0200

    premier commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+let score0El = document.getElementById('score--0');
+let score1El = document.getElementById('score--1');
+let diceEl = document.querySelector('.dice');
+
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
